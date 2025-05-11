fe1a6aa65936ecab5dc861db9109c305efd0dcf3
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2025-05-11 12:29:59 +0200

    feat: add restart option

--- script.js
+++ script.js
@@ -2,33 +2,28 @@ function createPlayer(name, token) {
     return { name, token };
 }
 
-const gameBoard = (function () {
+function createGameBoard() {
     const board = new Array(9);
-
-    function isWithinBounds(position) {
-        return position >= 0 && position < board.length;
-    }
 
     function getBoard() {
         return [...board];
     }
 
-    function getTokenAtOrDefault(position) {
-        return isWithinBounds(position) ? board[position] : null;
-    }
-
     function trySetTokenAt(token, position) {
-        if (!isWithinBounds(position) || board[position] !== undefined) {
+        if (position < 0 || position >= board.length || board[position] !== undefined) {
             return false;
         }
         board[position] = token;
         return true;
     }
 
-    return { getBoard, getTokenAtOrDefault, trySetTokenAt };
-})();
+    return {
+        getBoard,
+        trySetTokenAt
+    };
+}
 
-const game = (function (gameBoard, playerOne, playerTwo, onWinEventHandler, onTieEventHandler) {
+function createGame(gameBoard) {
     const WIN_POSITIONS = [
         [0, 1, 2],
         [3, 4, 5],
@@ -44,15 +39,32 @@ const game = (function (gameBoard, playerOne, playerTwo, onWinEventHandler, onTi
 
     let round = 0;
 
-    let gameFinished = false;
+    let playerOne = null;
+
+    let playerTwo = null;
+
+    let onGameFinished = null;
 
     function incrementRound() {
         round++;
     }
 
+    function setPlayerOne(player) {
+        playerOne = player;
+    }
+
+    function setPlayerTwo(player) {
+        playerTwo = player;
+    }
+
+    function setOnGameFinished(eventHandler) {
+        onGameFinished = eventHandler;
+    }
+
     function checkForWinner(token) {
+        const board = gameBoard.getBoard();
         for (const winPosition of WIN_POSITIONS) {
-            if (winPosition.every(position => gameBoard.getTokenAtOrDefault(position) === token)) {
+            if (winPosition.every(position => board[position] === token)) {
                 return true;
             }
         }
@@ -60,47 +72,90 @@ const game = (function (gameBoard, playerOne, playerTwo, onWinEventHandler, onTi
     }
 
     function playRound(position) {
-        if (gameFinished) return;
         const roundPlayer = round % 2 === 0 ? playerOne : playerTwo;
 
         if (gameBoard.trySetTokenAt(roundPlayer.token, position)) {
             if (checkForWinner(roundPlayer.token)) {
-                gameFinished = true;
-                onWinEventHandler(roundPlayer);
+                onGameFinished(roundPlayer);
             } else if (round === LAST_ROUND) {
-                gameFinished = true;
-                onTieEventHandler();
+                onGameFinished();
             }
 
             incrementRound();
         }
     }
 
-    return { playRound };
-})(
-    gameBoard,
-    createPlayer('Test 1', 'X'),
-    createPlayer('Test 2', 'O'),
-    (player) => console.log(player.name),
-    () => console.log('Tie')
-);
+    return { playRound, setPlayerOne, setPlayerTwo, setOnGameFinished };
+}
 
-const displayController = (function (document, gameBoard, game) {
-    const buttons = [...document.querySelectorAll('.grid-button')];
+function createDisplayController(document, gameBoard, game) {
+    let grid = null;
 
-    for (const button of buttons) {
-        const position = button.getAttribute('id');
-        button.addEventListener('click', () => {
-            game.playRound(position);
-            render();
-        });
+    let onReset = null;
+
+    const playersFormElement = document.querySelector('.players-form');
+
+    playersFormElement.addEventListener('submit', event => {
+        event.preventDefault();
+        const playerOneElement = document.querySelector('#player-one');
+        const playerTwoElement = document.querySelector('#player-two');
+        game.setPlayerOne(createPlayer(playerOneElement.value, 'X'));
+        game.setPlayerTwo(createPlayer(playerTwoElement.value, 'O'));
+        game.setOnGameFinished(renderResult);
+        playersFormElement.remove();
+        createGrid();
+    });
+
+    function createGrid() {
+        grid = document.createElement('div');
+        grid.classList.add('grid');
+        for (let i = 0; i < 9; i++) {
+            const button = document.createElement('button');
+            button.classList.add('grid-button');
+            button.addEventListener('click', () => {
+                game.playRound(i);
+                renderGrid();
+            });
+            grid.appendChild(button);
+        }
+        document.querySelector('body').appendChild(grid);
     }
 
-    function render() {
-        const board = gameBoard.getBoard();
-        for (let i = 0; i < board.length; i++) {
-            const token = board[i];
-            buttons[i].textContent = token;
+    function renderGrid() {
+        const buttons = [...document.querySelectorAll('.grid-button')];
+        if (buttons.length === 9) {
+            const board = gameBoard.getBoard();
+            for (let i = 0; i < board.length; i++) {
+                const token = board[i];
+                buttons[i].textContent = token;
+            }
         }
     }
-})(document, gameBoard, game);
+
+    function renderResult(winner = null) {
+        grid.remove();
+        const result = document.createElement('div');
+        winner === null ? result.textContent = 'Tie!' : result.textContent = `Winner: ${winner.name}`;
+        const resetButton = document.createElement('button');
+        resetButton.textContent = 'Restart game';
+        resetButton.addEventListener('click', () => onReset());
+        const body = document.querySelector('body');
+        body.appendChild(result);
+        body.appendChild(resetButton);
+    }
+
+    function setOnReset(eventHandler) {
+        onReset = eventHandler;
+    }
+
+    return { setOnReset };
+}
+
+function main(document) {
+    const gameBoard = createGameBoard();
+    const game = createGame(gameBoard);
+    const displayController = createDisplayController(document, gameBoard, game);
+    displayController.setOnReset(() => window.location.reload());
+}
+
+main(document);
